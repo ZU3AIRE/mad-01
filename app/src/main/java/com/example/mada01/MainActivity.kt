33dd1d512d2fda672a735247a2aa63db8e86efbd
Zubair Jamil <xubairjamil@gmail.com>
2024-07-16 10:10:25 +0500

    Reinitialized as empty project

--- app/src/main/java/com/example/mada01/MainActivity.kt
+++ app/src/main/java/com/example/mada01/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.mada01
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -12,8 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
-
-    val EMAIL_INP_REQ: Int  = 101;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,22 +22,7 @@ class MainActivity : AppCompatActivity() {
         }
         val btn = findViewById<Button>(R.id.btnMain)
         btn.setOnClickListener {
-            val intent = Intent(this, SecondaryActivity::class.java)
-            startActivityForResult(intent, EMAIL_INP_REQ)
-        }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == EMAIL_INP_REQ) {
-            if (resultCode == Activity.RESULT_OK) {
-                if (data != null) {
-                    val receivedData = data.getStringExtra("email")
-                    Toast.makeText(this,  receivedData, Toast.LENGTH_SHORT).show()
-
-                }
-            }
+            Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
         }
     }
 }
